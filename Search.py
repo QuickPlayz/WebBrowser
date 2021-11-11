0d063614fe26a4eb6c25b0970d37a2bee5142cbe
QuickPlayz <94027748+QuickPlayz@users.noreply.github.com>
2021-11-10 21:27:41 -0500

    Update Search.py

--- Search.py
+++ Search.py
@@ -5,30 +5,33 @@ url2 = 'https://www.youtube.com/results?search_query='
 url3 = 'https://duckduckgo.com/?q='
 S = '0'
 #variables that we need
-
+print('Made by @Quickplayz on GitHub')
+print('----------------------------')
 print('Where do you want to search')
 print('Type 0 to check out my Github')
 print('(1 = Google, 2 = Youtube, 3 = DuckDuckGo)')
+print('----------------------------')
 S = input()
+
 
 if S == '0':
     webbrowser.open('https://github.com/QuickPlayz')
     quit() #github
 
 if S == '1' :
-    print('What do you want to look up')
+    print('What do you want to look up (Google)')
     S1 = input() 
     webbrowser.open("".join([url1, S1 ]))
     quit() #google
 
 if S == '2' :
-    print('What do you want to look up')
+    print('What do you want to look up (Youtube)')
     S2 = input() 
     webbrowser.open("".join([url2, S2 ]))
     quit() #youtube
 
 if S == '3' :
-    print('What do you want to look up')
+    print('What do you want to look up (DuckDuckGo)')
     S3 = input() 
     webbrowser.open("".join([url3, S3 ]))
     quit() #duckduckgo
